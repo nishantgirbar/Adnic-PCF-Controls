3ad4234623fa2a6e48a7e3358079cccf869bbe07
nishant kumar <nishantgirbar@gmail.com>
2026-06-29 00:50:08 +0530

    Underwriter

---
+++ QuoteSummary/QuoteSummary/generated/ManifestTypes.d.ts
@@ -0,0 +1,14 @@
+/*
+*This is auto generated from the ControlManifest.Input.xml file
+*/
+
+// Define IInputs and IOutputs Type. They should match with ControlManifest.
+export interface IInputs {
+    quoterecordId: ComponentFramework.PropertyTypes.WholeNumberProperty;
+    quoteNumber: ComponentFramework.PropertyTypes.StringProperty;
+    apiUrl: ComponentFramework.PropertyTypes.StringProperty;
+}
+export interface IOutputs {
+    quoterecordId?: number;
+    quoteNumber?: string;
+}
